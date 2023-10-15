04c661dac2252acc4a8f9368bf5f7a2b6628a51b
realib1 <ibeevpersie@gmail.com>
2023-10-15 10:29:32 -0400

    Gracias

--- main.js
+++ main.js
@@ -2,13 +2,11 @@ const navLinks = document.querySelectorAll('#nav a');
 
 // Add an event listener to each link
 navLinks.forEach(link => {
-  link.addEventListener('click',  (event) => {
+  link.addEventListener('click', function (event) {
     // Remove the "active-link" class from all links
-    navLinks.forEach(link => link.classList.remove('active-link'));
+    navLinks.forEach(link => link.classList.remove('active'));
 
     // Add the "active-link" class to the link with id "active"
-    if (this.id === "active") {
-      this.classList.add('active-link');
-    }
+      this.classList.add('active');
   });
 });
